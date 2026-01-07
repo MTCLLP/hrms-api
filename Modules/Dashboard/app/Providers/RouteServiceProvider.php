6762be5d05d9c23931240b00ec3149e012c3580f
Apple <apple@Apples-MacBook-Pro.local>
2026-01-07 13:42:38 +0530

    changes

--- Modules/Dashboard/app/Providers/RouteServiceProvider.php
+++ Modules/Dashboard/app/Providers/RouteServiceProvider.php
@@ -47,7 +47,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('api')
             ->prefix('api')
-            ->name('api.')
             ->group(module_path($this->name, '/routes/api.php'));
     }
 }
